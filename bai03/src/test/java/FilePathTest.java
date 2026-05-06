b9d256263a52935e1581cba9999737d98c5887eb
thihuyenlybui080-spec <thihuyenlybui080@gmail.com>
2026-05-06 17:02:22 +0700

    bai04: Add Matrix Strategy + FIlePathTest to demo OS failure

---
+++ bai03/src/test/java/FilePathTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;import java.nio.file.Path;import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FilePathTest {
+    @Test
+    public void testPathWithSeparator(){
+        String path = "data" + File.separator + "input.txt";
+        assertTrue(path.contains("data"));
+        assertTrue(path.contains("input.txt"));
+    }
+
+    @Test
+    public void testNioPath(){
+        Path path = Paths.get("data", "input.txt");
+        assertTrue(path.toString().contains("input.txt"));
+    }
+
+    @Test
+    public void testHardcodedPath_WillFailOnWindows(){
+        String hardcodePath = "data/input.txt";
+        String correctPath = "data" + File.separator + "input.txt";
+        assertTrue(hardcodePath.equals(correctPath));
+
+    }
+}
